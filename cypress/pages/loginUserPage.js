060d38bff9b0c52bee0693d7017581eedf4b5418
hope <omid_mohamadyari@yahoo.com>
2022-12-02 21:07:31 +0330

    - login user page

--- cypress/pages/loginUserPage.js
+++ cypress/pages/loginUserPage.js
@@ -2,6 +2,22 @@ require('cypress-xpath');
 
 class loginUserPage {
     elements = {
-
+        emailLoginPage:()=>cy.get('#input-1'),
+        passwordLoginPage:()=>cy.get('#input-2'),
+        loginPageError:()=>cy.get('.css-tw8q33'),
+        loginPageSubmit:()=>cy.get('.css-1bxciiq'),
     }
+
+    typeEmailLoginPage(email) {
+        this.elements.emailLoginPage().type(email);
+    }
+    typePasswordLoginPage(password) {
+        this.elements.passwordLoginPage().type(password);
+    }
+    ClickLoginPageSubmit() {
+        this.elements.loginPageSubmit().click();
+    }
+
 }
+
+module.exports = new loginUserPage();
